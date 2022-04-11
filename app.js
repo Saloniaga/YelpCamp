78b8d1d7a5e547386ec4219c9ccc7d8a2dfe21b6
Saloniaga <sagarwal9475@gmail.com>
2022-04-12 00:45:32 +0530

    add start script

--- app.js
+++ app.js
@@ -116,7 +116,7 @@ app.use((err, req, res, next)=>{
 //     await camp.save()
 //     res.send(camp)
 // })
-
-app.listen(3000, ()=>{
+const port= process.env.PORT || 3000;
+app.listen(port, ()=>{
     console.log('listening on port 3000...')
 })
